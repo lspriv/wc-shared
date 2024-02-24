987fe9b3587abc7a9ae26feaf4665a3a4eacea90
lspriv <976928310@qq.com>
2024-02-24 17:05:36 +0800

    fix(*): 调整

--- src/date.ts
+++ src/date.ts
@@ -4,8 +4,9 @@
  * See File LICENSE for detail or copy at https://opensource.org/licenses/MIT
  * @Description: Description
  * @Author: lspriv
- * @LastEditTime: 2024-02-24 12:17:11
+ * @LastEditTime: 2024-02-24 16:56:58
  */
+
 export const DATE_SPLIT = /[ -\/TZ:\.]/;
 export const DATE_TIME_PATTERN = /^\d{4}[-\/]\d{1,2}[-\/]\d{1,2}[ T]\d{1,2}:\d{1,2}:\d{1,2}(\.\d{3})?Z?$/;
 export const DATE_PATTERN = /^\d{4}[-\/]\d{1,2}[-\/]\d{1,2}$/;
@@ -15,13 +16,13 @@ export const dateFormatStr = (str: string): string | null => {
   if (DATE_TIME_PATTERN.test(str)) {
     splits = str.split(DATE_SPLIT);
     const [year, month, day, hour, minute, second] = splits;
-    return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+    return `${year}/${month}/${day} ${hour}:${minute}:${second}`;
   }
 
   if (DATE_PATTERN.test(str)) {
     splits = splits || str.split(DATE_SPLIT);
     const [year, month, day] = splits;
-    return `${year}-${month}-${day} 00:00:00`;
+    return `${year}/${month}/${day} 00:00:00`;
   }
 
   return null;
